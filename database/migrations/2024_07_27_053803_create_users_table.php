4fb1099b5e7c7f64ee063c03e000d2520b29811f
der1der1 <deniel87deniel87@gmail.com>
2025-05-26 21:07:04 +0800

    feature: create filament admin

--- database/migrations/2024_07_27_053803_create_users_table.php
+++ database/migrations/2024_07_27_053803_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('account', 255)->unique()->nullable();
+            $table->string('email', 255)->unique()->nullable();
             $table->string('password',  255)->nullable();
             $table->string('prvilige', 45)->nullable();
             $table->string('name', 45)->nullable();
