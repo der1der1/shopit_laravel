f275610c18794fa66e66a1da6e18939d60b8f953
der1der1 <deniel87deniel87@gmail.com>
2025-07-24 22:54:38 +0800

    fix: add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {        
         // $this->call(MarqeeTableSeeder::class);
-        // $this->call(ProductsTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
         // $this->call(SessionsTableSeeder::class);
-        $this->call(MailListSeeder::class);
+        // $this->call(MailListSeeder::class);
         // $this->call(usersSeeder::class);
     }
 }
